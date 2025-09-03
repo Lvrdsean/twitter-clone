cc4cb972db8f8ffde3505a83b765cb2039c55135
Lvrdsean <seanny813@optonline.net>
2025-09-03 19:51:27 -0400

    Update Home Screen Footer

--- frontend/src/screens/home/components/Footer.tsx
+++ frontend/src/screens/home/components/Footer.tsx
@@ -3,18 +3,18 @@ import "./Footer.scss";
 const Footer = () => {
     return (
         <footer>
-            <p className={`p3-r ${"home__footer_text"}`}>
-                <span className="home__footer_link">Terms of Service</span>
+            <p className={`p3-r ${"footer__text"}`}>
+                <span className="footer__link">Terms of Service</span>
                 <span>|</span>
-                <span className="home__footer_link">Privacy Policy</span>
+                <span className="footer__link">Privacy Policy</span>
                 <span>|</span>
-                <span className="home__footer_link">Cookie Policy</span>
+                <span className="footer__link">Cookie Policy</span>
                 <span>|</span>
-                <span className="home__footer_link">Accessibility</span>
+                <span className="footer__link">Accessibility</span>
                 <span>|</span>
-                <span className="home__footer_link">Ads Info</span>
+                <span className="footer__link">Ads Info</span>
                 <span>|</span>
-                <span className="home__footer_link">More ...</span>
+                <span className="footer__link">More ...</span>
                 <span>|</span>
                 <span>© 2025 LVRD Corp.</span>
             </p>
